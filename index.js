45527799c8c2a99089d11d882968271bda243b27
= <=>
2024-10-25 02:14:07 +0500

    afs

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 const patientRoutes = require("./routes/patient");
@@ -9,7 +8,15 @@ const rootUser = require("./routes/rootUser");
 const { notFound, errorHandler } = require("./middlewares/error");
 const protectRoute = require("./middlewares/protectRoute");
 
-dotenv.config();
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect("mongodb+srv://workcorp:workcorp@cluster0.xvmzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 connectDB();
 
 const app = express();
@@ -29,5 +36,5 @@ app.use("/api/rootuser", rootUser);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
